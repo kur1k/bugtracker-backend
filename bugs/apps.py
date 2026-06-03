161146aa9cfb39b562ae40f2a6a81a71562b02f2
igor <igorson2002@gmail.com>
2026-06-03 10:22:47 +0300

    fix apps.py

--- bugs/apps.py
+++ bugs/apps.py
@@ -3,15 +3,3 @@ from django.apps import AppConfig
 class BugsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bugs'
-
-    def ready(self):
-        try:
-            from bugs.models import User
-            if not User.objects.filter(username='admin').exists():
-                User.objects.create_superuser('admin', 'admin@mail.ru', 'Admin1234!', role='admin')
-            if not User.objects.filter(username='tester').exists():
-                User.objects.create_user('tester', 'tester@mail.ru', 'Tester1234!', role='tester')
-            if not User.objects.filter(username='developer').exists():
-                User.objects.create_user('developer', 'dev@mail.ru', 'Developer1234!', role='developer')
-        except Exception:
-            pass
